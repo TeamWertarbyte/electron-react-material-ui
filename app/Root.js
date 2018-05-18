09265409a52aeb9991aa676d190fb436467d195e
Sascha Becker <s.becker@wertarbyte.com>
2018-05-18 17:42:24 +0200

    Remove flow

--- app/Root.js
+++ app/Root.js
@@ -1,18 +1,12 @@
-// @flow
 import React from 'react'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router'
 import routes from './routes'
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 const theme = createMuiTheme()
-type
-  RootType = {
-  store: {},
-  history: {}
-}
 
-export default function Root ({store, history}: RootType) {
+export default function Root ({store, history}) {
   return (
     <Provider store={store}>
       <MuiThemeProvider theme={theme}>
--- app/menu.js
+++ app/menu.js
@@ -1,10 +1,9 @@
-// @flow
 import { app, BrowserWindow, Menu, shell } from 'electron'
 
 export default class MenuBuilder {
   mainWindow: BrowserWindow
 
-  constructor (mainWindow: BrowserWindow) {
+  constructor (mainWindow) {
     this.mainWindow = mainWindow
   }
 
